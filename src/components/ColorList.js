8f675494d2005dd3a8b7f53a7ee45eef181a0a36
SheeTabz <wmuriithitabitha@gmail.com>
2022-09-20 19:52:07 +0300

    list & keys practice

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -8,18 +8,17 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
-
+ const mapping = colors.map(item => {
+  return <li key={item} style ={{color: item}}>{item}</li>
+ })
   return (
-    <div>
+    <React.Fragment>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {mapping}
+     
       </ol>
-    </div>
+    </React.Fragment>
   );
 }
 
